470a9b94c0239d9bd42d8ced6c794b593fa2cb0f
Dawid Maliszewski <dawid@thedavesky.com>
2019-07-28 17:00:46 +0200

    Fix no timeout bug and change name

--- ambilight-screen.py
+++ ambilight-screen.py
@@ -5,7 +5,7 @@ import colorsys
 import requests
 
 # Program start information
-print('\033[95m\033[1m--- OpenHAB Ambilight Screen v0.1.0 ---\033[0m\n\033[94m\033[1m[' +
+print('\033[95m\033[1m--- openHAB2 Ambilight Screen v0.1.0 ---\033[0m\n\033[94m\033[1m[' +
       time.strftime('%H:%M:%S') +
       '] Started program.\033[0m')
 
@@ -17,17 +17,18 @@ try:
             with open('config.json') as json_file:
                 json_data = json.load(json_file)
                 while True:
-                    # Get average color from main screen
-                    avg_color = ImageGrab.grab().resize((1, 1), Image.ANTIALIAS).getpixel((0, 0))
+                    # Get average colour from main screen
+                    avg_colour = ImageGrab.grab().resize((1, 1), Image.ANTIALIAS).getpixel((0, 0))
 
                     # Convert RGB to HSV
-                    h, s, v = colorsys.rgb_to_hsv((avg_color[0]/255.0), (avg_color[1]/255.0), (avg_color[2]/255.0))
-                    color_data = (str(h*360.0)+','+str(s*100.0)+','+str(v*100.0))
+                    h, s, v = colorsys.rgb_to_hsv((avg_colour[0]/255.0), (avg_colour[1]/255.0), (avg_colour[2]/255.0))
+                    colour_data = (str(h*360.0)+','+str(s*100.0)+','+str(v*100.0))
 
-                    # Send HSV color to openHAB server
+                    # Send HSV colour to openHAB server
                     r = requests.post(json_data['openhab_url']+'/rest/items/'+json_data['openhab_item_name'],
-                                      data=color_data,
-                                      auth=(json_data['openhab_username'], json_data['openhab_password']))
+                                      data=colour_data,
+                                      auth=(json_data['openhab_username'], json_data['openhab_password']),
+                                      timeout=2)
                     
                     # Page not found error
                     if r.status_code == 404:
@@ -44,6 +45,7 @@ try:
                               'Check username and password in config file.\033[0m')
                         time.sleep(1)
                         break
+
         # Server SSL error
         except requests.exceptions.SSLError:
             print('\033[91m\033[1m['+time.strftime('%H:%M:%S') +
@@ -54,14 +56,14 @@ try:
         # Server connection error
         except requests.exceptions.ConnectionError:
             print('\033[91m\033[1m['+time.strftime('%H:%M:%S') +
-                  '] Server connection error. Check your internet connection or URL in config file.\033[0m')
+                  '] Server connection error. Check your connection with server or URL in config file.\033[0m')
             time.sleep(1)
             continue
 
 # JSON file not found error
 except FileNotFoundError:
     print('\033[91m\033[1m['+time.strftime('%H:%M:%S') +
-          '] No config.json file! Create it using config.json.sample as example.' +
+          '] No config.json file! Create it using config.json.sample as example. ' +
           'More informations in README file.\033[0m\n\033[94m\033[1m[' +
           time.strftime('%H:%M:%S') +
           '] Stopped program.\033[0m')
